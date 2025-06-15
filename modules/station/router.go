0f320f18ddc88a329899563445fff2bb51e295e9
Fahril Hadi <fahrilhadi77@gmail.com>
2025-06-15 21:05:42 +0700

    API Stasiun - Menghubungkan Router dan Service

--- modules/station/router.go
+++ modules/station/router.go
@@ -3,8 +3,20 @@ package station
 import "github.com/gin-gonic/gin"
 
 func Initiate(router *gin.RouterGroup)  {
+	stationService := NewService()
+
 	station := router.Group("/station")
 	station.GET("", func(ctx *gin.Context) {
 		// code service
+		GetAllStation(c, stationService)
 	})
+}
+
+func GetAllStation(c *gin.Context, service Service)  {
+	datas, err := service.GetAllStation()
+	if err != nil {
+		// handle error
+	}
+
+	// balikin response
 }
